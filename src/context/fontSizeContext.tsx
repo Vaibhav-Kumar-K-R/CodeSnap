25e39e1a794c169f35e2159b34fa07ec6a83747b
Vaibhav-Kumar-K-R <vaibhavkumar.k.r.cnr@gmail.com>
2025-11-05 10:45:36 +0530

    Added context for state management

---
+++ src/context/fontSizeContext.tsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from 'react';
+
+type FontSizeContextType = {
+  fontSize: number;
+  setFontSize: (fontSize: number) => void;
+};
+
+export const fontSizeContext = createContext<FontSizeContextType | undefined>(
+  undefined
+);
+
+export default function FontSizeProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [fontSize, setFontSize] = useState(12);
+  return (
+    <fontSizeContext.Provider value={{ fontSize, setFontSize }}>
+      {children}
+    </fontSizeContext.Provider>
+  );
+}
