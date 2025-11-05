f0a390e4d5dfe96615ec5baf9a5c8d96e2fdfcb8
Vaibhav-Kumar-K-R <vaibhavkumar.k.r.cnr@gmail.com>
2025-11-05 10:46:08 +0530

    Added custom hook for enhanced functionality

---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,10 @@
+import { DarkModeContext } from '@/context/darkModeContext';
+import { useContext } from 'react';
+
+export const useDarkMode = () => {
+  const context = useContext(DarkModeContext);
+  if (!context) {
+    throw new Error('darkModeContext is possibly undefined!');
+  }
+  return context;
+};
